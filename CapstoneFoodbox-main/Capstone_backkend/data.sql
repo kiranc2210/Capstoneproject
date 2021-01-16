aae920cc26d9e3b113404cecaae476cce4d50143
kiranc2210 <kiranc2210@gmail.com>
2021-01-17 00:51:52 +0530

    final push

---
+++ CapstoneFoodbox-main/Capstone_backkend/data.sql
@@ -0,0 +1,12 @@
+
+INSERT into ADMIN(ID,ADMIN_NAME,ADMIN_PWD) VALUES(1001, 'KIRAN', 'KM@2229');
+
+
+INSERT into USER(ID,EMAILID,NAME,PWD) VALUES(2001, 'joker@gmail.com', 'Joaquin Phoenix', 236456);
+INSERT into USER(ID,EMAILID,NAME,PWD) VALUES(2002, 'ironman@gmail.com', 'Robert Doweny jr', 115975);
+INSERT into USER(ID,EMAILID,NAME,PWD) VALUES(2003, 'captainamerica@gmail.com', 'crise evens', 159753);
+INSERT into USER(ID,EMAILID,NAME,PWD) VALUES(2004, 'jumanji@gmail.com', 'Dwayne johnson', 456852);
+INSERT into USER(ID,EMAILID,NAME,PWD) VALUES(2005, 'johnwick@gmail.com', 'keanu reeves', 852654);
+INSERT into USER(ID,EMAILID,NAME,PWD) VALUES(2006, 'Mangatha@gmail.com', 'Thala Ajith', 222290);
+INSERT into USER(ID,EMAILID,NAME,PWD) VALUES(2007, 'Dboss@gmail.com', 'Darshan', 222291);
+
---
+++ CapstoneFoodbox-main/Capstone_backkend/src/main/java/com/food/box/Capstone4Application.java
@@ -0,0 +1,33 @@
+package com.food.box;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.food.box.controller.AdminController;
+import com.food.box.model.Admin;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@ComponentScan("com.food.box")
+@EnableSwagger2
+public class Capstone4Application {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Capstone4Application.class, args);
+		AdminController adminc= new AdminController();
+		
+		System.out.println(adminc.addAdminlogin(new Admin("KIRAN","KM@2229")));
+	}
+
+	public Docket FoodBoxApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.food.box")).build();	
+	}
+
+
+}
